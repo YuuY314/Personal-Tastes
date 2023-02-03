73106a8a9b15dda04088daa13f0f3639a82b79be
Yu <rafaelyu0202@gmail.com>
2023-02-03 19:23:28 -0300

    Criação do Repositório

---
+++ README.md
@@ -0,0 +1,13 @@
+# Personal Tastes
+
+Plataforma para hospedar todas as suas obras favoritas.
+
+No momento não consegue salvar os dados inseridos. De resto, tudo está funcionando, eu espero.
+
+Link para acessar:
+
+CodePen: https://codepen.io/YuuY314/pen/GRBPYVr
+
+## Imersão Dev
+
+Deixando claro que este projeto é uma das atividades da Imersão Dev da Alura.
---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+const nameInput = document.querySelector("#name");
+const imageInput = document.querySelector("#image");
+const categoryComboBox = document.querySelector("#category");
+const addBtn = document.querySelector("#add-btn");
+
+const personalTasteCategories = document.querySelectorAll(".personal-taste-category");
+
+let personalTastes = [];
+
+let index = -1;
+
+addBtn.addEventListener("click", () => {
+    if(nameInput.value == "" || imageInput.value == ""){
+        alert("Preencha todos os campos");
+    } else if(!imageInput.value.endsWith("jpg") && !imageInput.value.endsWith("png") && !imageInput.value.endsWith("jpeg") && !imageInput.value.endsWith("webp") && !imageInput.value.endsWith("svg")){
+        alert("O link da imagem precisa terminar exatemente com: .jpg / .png / .jpeg / .webp / .svg");
+    } else {
+        index++;
+        personalTastes.push({
+            name: nameInput.value,
+            image: imageInput.value,
+            category: categoryComboBox.value,
+            id: index
+        });
+
+        nameInput.value = "";
+        imageInput.value = "";
+
+        loadPersonalTastes();
+    }
+});
+
+function loadPersonalTastes(){
+    personalTasteCategories.forEach(personalTasteCategory => {
+        const personalTasteCards = document.querySelectorAll(".personal-taste-card");
+        personalTasteCards.forEach(personalTasteCard => {
+            personalTasteCard.remove();
+        });
+    });
+
+    for(var i = 0; i < personalTastes.length; i++){
+        const personalTasteCard = document.createElement("div");
+        personalTasteCard.classList.add("personal-taste-card");
+        personalTasteCard.id = personalTastes[i].id;
+    
+        const personalTasteImage = document.createElement("img");
+        personalTasteImage.src = personalTastes[i].image;
+    
+        const personalTasteName = document.createElement("p");
+        personalTasteName.textContent = personalTastes[i].name;
+    
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("remove-btn");
+        removeBtn.innerHTML = "<i class='fa-solid fa-xmark'>";
+    
+        personalTasteCard.appendChild(personalTasteImage);
+        personalTasteCard.appendChild(personalTasteName);
+        personalTasteCard.appendChild(removeBtn);
+    
+        personalTasteCategories.forEach(personalTasteCategory => {
+            if(personalTastes[i].category == personalTasteCategory.id){
+                personalTasteCategory.style.display = "flex";
+                personalTasteCategory.appendChild(personalTasteCard);
+            }
+        });
+    }
+}
+
+document.addEventListener("click", (e) => {
+    let element = e.target;
+    let parentElement = element.closest("div");
+
+    if(element.classList.contains("remove-btn")){
+        parentElement.remove();
+        console.log(personalTastes);
+        personalTastes = personalTastes.filter(personalTaste => personalTaste.id != parentElement.id);
+        console.log(personalTastes);
+
+        personalTasteCategories.forEach(personalTasteCategory => {
+            let numChilds = personalTasteCategory.childElementCount;
+            if(numChilds <= 1){
+                personalTasteCategory.style.display = "none";
+            }
+        });
+    }
+});
